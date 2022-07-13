6dbb9ac4131b592fcb5e796026d99fb097ddbc77
dxxbletriiiple <dxxbletriiiple@gmail.com>
2022-07-13 16:50:30 +0300

    done: chapter 1 counter

---
+++ day_00/chapter_1/counter.js
@@ -0,0 +1,11 @@
+function makeCount() {
+	let count = 0;
+	return function () {
+		return count++ * 3;
+	};
+}
+let counter = makeCount();
+console.log(counter()); //0
+console.log(counter()); //3
+console.log(counter()); //6
+console.log(counter()); //9
